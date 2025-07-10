9bc8ff9b56dac43af5a5564d265ffe822e5ce2a9
Natalie B. <22222885+espeon@users.noreply.github.com>
2025-07-10 09:25:17 -0500

    Remove GrainOverlay from CV page and comment out alt grain in component

--- src/components/GrainOverlay.tsx
+++ src/components/GrainOverlay.tsx
@@ -109,7 +109,7 @@ export function GrainOverlay({
       />
 
       {/* Alternative CSS-only grain for better performance */}
-      <div
+      {/* <div
         className="pointer-events-none fixed inset-0"
         style={{
           opacity: opacity * 0.5,
@@ -117,7 +117,7 @@ export function GrainOverlay({
           backgroundImage: `url("data:image/svg+xml,%3Csvg viewBox='0 0 200 200' xmlns='http://www.w3.org/2000/svg'%3E%3Cfilter id='noiseFilter'%3E%3CfeTurbulence type='turbulence' baseFrequency='${baseFrequency}' numOctaves='4' stitchTiles='stitch'/%3E%3C/filter%3E%3Crect width='100%25' height='100%25' filter='url(%23noiseFilter)' opacity='0.5'/%3E%3C/svg%3E")`,
           backgroundRepeat: "repeat",
         }}
-      />
+      /> */}
     </>
   );
 }
