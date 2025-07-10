80d02a990dcb6b2a4e70b247b44c4799aa812ffe
Natalie B. <22222885+espeon@users.noreply.github.com>
2025-07-10 08:49:50 -0500

    Add IconRow component and initial CV page

---
+++ src/components/icon-row.tsx
@@ -0,0 +1,32 @@
+import { IoLogoTwitch } from "react-icons/io";
+import {
+  FaGithub,
+  FaTwitch,
+  FaBluesky,
+  FaDiscord,
+  FaLastfm,
+} from "react-icons/fa6";
+
+export function IconRow() {
+  let icons = [
+    { i: <FaGithub />, url: "https://github.com/espeon" },
+    { i: <FaBluesky />, url: "https://bsky.app/profile/natalie.sh" },
+    { i: <IoLogoTwitch />, url: "https://twitch.tv/uxieq" },
+    { i: <FaLastfm />, url: "https://last.fm/user/kanb" },
+    { i: <FaDiscord />, url: "https://nat.vg/discord" },
+  ];
+  return (
+    <div className="flex py-2" style={{ fontSize: "1.75rem" }}>
+      {icons.map((e) => {
+        return (
+          <div
+            className="pr-[.25em] text-gray-700 dark:text-gray-400"
+            key={e.url}
+          >
+            <a href={e.url}>{e.i}</a>
+          </div>
+        );
+      })}
+    </div>
+  );
+}
